d69939f8bc8bba2f141c9791fd4b17446160a2f8
Dikshant Patodia <dikshantpatodia@gmail.com>
2019-09-05 00:17:42 +0530

    lint files

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": [
+    "airbnb-base",
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "rules": {
+    "no-param-reassign": 0
+  }
+};
